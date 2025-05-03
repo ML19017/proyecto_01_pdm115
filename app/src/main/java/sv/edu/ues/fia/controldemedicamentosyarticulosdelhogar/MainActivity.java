e1de20354976bf40d75c7261483ab73a31b745e6
ML19017 <ml19017@ues.edu.sv>
2025-05-03 10:26:24 -0600

    Update MainActivity.java
    Se crean las prefenrencias para solo se ejecute la primera vez la creación de la base de datos.

--- app/src/main/java/sv/edu/ues/fia/controldemedicamentosyarticulosdelhogar/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/controldemedicamentosyarticulosdelhogar/MainActivity.java
@@ -1,6 +1,8 @@
 package sv.edu.ues.fia.controldemedicamentosyarticulosdelhogar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SQLiteDatabase db = new DataBaseHelper(this).getWritableDatabase();
-        DataBaseHelper DBHelper = new DataBaseHelper(this);
-        DBHelper.onCreate(db);
-        DBHelper.close();
+
+        SharedPreferences preferencias = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+        boolean primeraVez =  preferencias.getBoolean("primera_vez", true);
+
+        if(primeraVez) {
+            SQLiteDatabase db = new DataBaseHelper(this).getWritableDatabase();
+            DataBaseHelper DBHelper = new DataBaseHelper(this);
+            DBHelper.onCreate(db);
+            DBHelper.close();
+            SharedPreferences.Editor editor = preferencias.edit();
+            editor.putBoolean("primera_vez", false);
+            editor.apply();
+        }
         // Redirigir al LoginActivity
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish(); // Finaliza MainActivity para que no se pueda volver atrás
     }
+
+
+
+
 }
